e552fecf8234d08309fc0bf6a78577a185b327ca
Ketki Mahajan <mahajanketki24@gmail.com>
2018-10-20 19:19:18 -0500

    Adding changes for hashHelper and compareTo

--- src/axe170009/Hashing.java
+++ src/axe170009/Hashing.java
@@ -5,29 +5,20 @@
  */
 package axe170009;
 
-import java.util.Arrays;
+public class Hashing<K extends Comparable<? super K>> {
+    int size;
+    int capacity;
+    int loadFactor;
+    HashNode<K>[] table;
 
-public class Hashing<K> {
-	
-	//HashNode class
-	private class HashNode<T>{
-		T key;
-		boolean isDeleted;
-		boolean isFree = true;
-		
-		//constructor of HashNode class
-		public HashNode(T key) {
-			this.key = key;
-			this.isDeleted = false;
-			this.isFree = false;
-		}
-	}
-	
-	HashNode<K>[] table;
-	int size;
-	int capacity;
-	int loadFactor;
-	
+    /*This function ensures that hashCodes that differ only by
+        constant multiples at each bit position have a bounded
+        number of collisions (approximately 8 at default load factor).*/
+    private static int hash(int h) {
+        h ^= (h >>> 20) ^ (h >>> 12);
+        return h ^ (h >>> 7) ^ (h >>> 4);
+    }
+
 	public Hashing() {
 		capacity = 32;
 		size = 0;
@@ -37,53 +28,51 @@ public class Hashing<K> {
 			table[i] = null;
 		}
 	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + capacity;
-		result = prime * result + size;
-		result = prime * result + Arrays.hashCode(table);
-		return result;
+
+    public static void main(String[] args) {
+        // driver methods
+        Hashing<Integer> table = new Hashing<>();
+        System.out.println(table.RobinHoodAdd(1000));
+        System.out.println(table.RobinHoodAdd(1001));
+        System.out.println(table.RobinHoodAdd(10));
+        System.out.println(table.RobinHoodAdd(160));
+
+        System.out.println(table.contains(1001));
+        System.out.println(table.remove(1000));
+        System.out.println(table.size);
+    }
+
+    private static int indexFor(int h, int length) {
+        return h & (length-1);
 	}
 
-	private static int hash(int h) {
-		/*This function ensures that hashCodes that differ only by
-		constant multiples at each bit position have a bounded
-		number of collisions (approximately 8 at default load factor).*/
-		h ^= (h >>> 20) ^ (h >>> 12);
-		return h ^ (h >>> 7) ^ (h >>> 4);
-	}
-	
-	private static int indexFor(int h, int length) {
-		return h & (length-1);
-	}
-	
-	private int find(K x) {
-		int spot = 0, hashValue = 0;
-		HashNode<K> curNode;
-		while(true) {
-			hashValue = indexFor(hash(x.hashCode()), this.capacity);
-			curNode = this.table[hashValue];
-			
-			if(curNode == null || curNode.key.equals(x) || curNode.isFree) {
-				return hashValue;
-			}
-			else if(curNode.isDeleted) {
+    public int hashHelper(K x) {
+        return indexFor(hash(x.hashCode()), this.capacity);
+    }
+
+    private int find(K x) {
+        int k = 0, ik = 0; //ik is hashValue
+        HashNode<K> curNode;
+        while(true) {
+            ik = (k + hashHelper(x)) % this.capacity;
+            curNode = this.table[ik];
+
+            if (curNode == null || (curNode.key.compareTo(x) == 0) || curNode.isFree) {
+                return ik;
+            } else if(curNode.isDeleted) {
 				break;
 			}
 			else {
-				spot++;
-			}
-		}
-		int xSpot = hashValue;
-		while(true) {
-			spot++;
-			if(curNode.key.equals(x)) {
-				return hashValue;
-			}
-			if(curNode.isFree) {
+                k++;
+            }
+        }
+        int xSpot = ik;
+        while (true) {
+            k++;
+            if (curNode.key.compareTo(x) == 0) {
+                return ik;
+            }
+            if(curNode.isFree) {
 				return xSpot;
 			}
 		}
@@ -92,17 +81,17 @@ public class Hashing<K> {
 	public boolean contains(K x) {
 		int location = find(x);
 		HashNode<K> node = this.table[location];
-		if(node != null && node.key.equals(x))return true;
-		return false;
-	}
-	
-	public K remove(K x) {
-		int location = find(x);
-		if(this.table[location].key.equals(x)) {
-			HashNode<K> result = this.table[location];
-			this.table[location].isDeleted = true;
-			return result.key;
-		}
+        return node != null && (node.key.compareTo(x) == 0);
+    }
+
+    public K remove(K x) {
+        int location = find(x);
+        if (this.table[location].key.compareTo(x) == 0) {
+            HashNode<K> result = this.table[location];
+            this.table[location].isDeleted = true;
+            this.size--;
+            return result.key;
+        }
 		return null;
 	}
 	
@@ -114,12 +103,13 @@ public class Hashing<K> {
 			HashNode<K> node = this.table[location];
 			if(node == null || node.isFree || node.isDeleted) {
 				this.table[location] = new HashNode<K>(x);
-				return true;
-			}
+                size++;
+                return true;
+            }
 			else if(displacement(this.table[location].key, location) >= disp) {
-				disp+=1;
-				location = (location+1)%this.capacity;
-			}
+                disp += 1;
+                location = (location + 1) % this.capacity;
+            }
 			else {
 				K temp = x;
 				x = this.table[location].key;
@@ -129,25 +119,29 @@ public class Hashing<K> {
 			}
 		}
 	}
-	
-	public boolean HopScotchAdd(K x) {
-		return false;
-	}
+
 	
 	// Calculate displacement of x from its ideal location of h( x )
 	private int displacement(K x, int location) {
-		int hashValue = indexFor(hash(x.hashCode()), this.capacity);
-		return (location >= hashValue) ? (location - hashValue) : (this.capacity + location - hashValue);
-	}
-	
-	public static void main(String[] args) {
-		// driver methods
-		Hashing<Integer> table = new Hashing<Integer>();
-		System.out.println(table.RobinHoodAdd(1000));
-		System.out.println(table.contains(1001));
-	}
-	
-	public static<T> int distinctElements(T[ ] arr) {
+        int hashValue = hashHelper(x);
+        return (location >= hashValue) ? (location - hashValue) : (this.capacity + location - hashValue);
+    }
+
+    //HashNode class
+    private class HashNode<T> {
+        T key;
+        boolean isDeleted;
+        boolean isFree = true;
+
+        //constructor of HashNode class
+        public HashNode(T key) {
+            this.key = key;
+            this.isDeleted = false;
+            this.isFree = false;
+        }
+    }
+
+    public static<T> int distinctElements(T[ ] arr) {
 		return 0;	
 	}
 
